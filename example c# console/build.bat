32aeef6dc53b1abe7007a33d555f1ccc509dc7ca
Hardik Singnath <hardiksingnath@gmail.com>
2026-04-29 16:22:12 +0530

    Final sync: Update client binaries and project structure

--- example c# console/build.bat
+++ example c# console/build.bat
@@ -1,25 +1,10 @@
 @echo off
-set CSC="C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe"
-
-if not exist %CSC% (
-    set CSC="C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe"
-)
-
-if not exist %CSC% (
-    echo Error: .NET Framework compiler (csc.exe) not found!
-    pause
-    exit /b 1
-)
+set "CSC=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe"
+if not exist "%CSC%" set "CSC=C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe"
 
 echo Compiling AuthCore Console Example...
+"%CSC%" /nologo /out:AuthCoreConsole.exe /reference:System.Management.dll /reference:System.Net.Http.dll /reference:System.Web.Extensions.dll /reference:System.Windows.Forms.dll Program.cs Sdk.cs Hwid.cs
 
-%CSC% /nologo /out:AuthCoreConsole.exe /reference:System.Management.dll /reference:System.Net.Http.dll /reference:System.Web.Extensions.dll /reference:System.Windows.Forms.dll Program.cs Sdk.cs Hwid.cs
-
-if %ERRORLEVEL% == 0 (
-    echo.
-    echo Build successful! Run AuthCoreConsole.exe to test.
-) else (
-    echo.
-    echo Build failed.
-)
+if %ERRORLEVEL% EQU 0 echo Build successful!
+if %ERRORLEVEL% NEQ 0 echo Build failed.
 pause
